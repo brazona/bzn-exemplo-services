e2b210e6c7aee4a0948d77d6c092fd67e322ff21
Cezar Silva <cezarfelipe2008@outlook.com>
2025-05-07 13:10:16 -0300

    test: contract-first exemplo services

--- bzn-exemplo-services-profiles/src/main/java/br/brazona/bzn_exemplo_services_profiles/infra/repositories/ProfilesRepository.java
+++
@@ -1,9 +0,0 @@
-package br.brazona.bzn_exemplo_services_profiles.infra.repositories;
-
-import br.brazona.bzn_exemplo_services_profiles.infra.entities.RolesEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfilesRepository extends CrudRepository<RolesEntity, Long> {
-}
